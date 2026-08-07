213ecb14b04b2a179b37b5ca3ded7198ab24167e
Duc Tien <boyndt1904@gmail.com>
2026-08-07 10:50:51 +0700

    upload

--- backend/database/myconnection.php
+++ backend/database/myconnection.php
@@ -1,7 +1,7 @@
 <?php
 class Database {
     private $host = "localhost";
-    private $port = 3307;
+    private $port = 3306;
     private $username = "root";
     private $password = "";
     private $database = "doc_truyen_web";
